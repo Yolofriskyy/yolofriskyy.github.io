2eaf218f6ef9ea19303c3196092c489843c1b96d
Harry Simmons <152905223+Yolofriskyy@users.noreply.github.com>
2024-10-01 18:04:04 +0100

    Update send_email.js

--- assets/js/send_email.js
+++ assets/js/send_email.js
@@ -1,7 +1,7 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting the default way
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this) // Replace with your EmailJS service and template IDs
+    emailjs.sendForm('service_amm2z2v', 'template_7w9zmnk', this) // Replace with your EmailJS service and template IDs
         .then(function() {
             alert('Message sent successfully!');
             document.getElementById('contact-form').reset(); // Reset the form after sending
